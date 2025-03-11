19e3f52039a76f359b9e155cdd22d5ea74f79a49
wyrenside <haciyev004@icloud.com>
2025-03-11 23:18:44 +0400

    fifth task

---
+++ task 5/task 5/Program.cs
@@ -0,0 +1,22 @@
+﻿ 
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+int sum = 0;
+if
+    (a % 5 == 0)
+{
+    sum += a;
+}
+if 
+    (b % 5 == 0)
+{
+    sum += b;
+}
+if
+    (c % 5 == 0)
+{
+    sum += c;
+}
+Console.WriteLine(sum);
