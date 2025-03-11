e54299d377b9de7e323286f2d203da96e92002e8
wyrenside <haciyev004@icloud.com>
2025-03-12 01:34:32 +0400

    eleventh task

---
+++ task 11/task 10/task 10/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] numbers = new int[10];
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = int.Parse(Console.ReadLine());
+}
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] % 2 == 0)
+    {
+        Console.WriteLine(numbers[i]);
+    }
+}
