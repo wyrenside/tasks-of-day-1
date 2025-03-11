8af4e2c963a775be711b820828ff42a7cf62e4ad
wyrenside <haciyev004@icloud.com>
2025-03-11 23:17:12 +0400

    third task

---
+++ task 3/task 3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.Design;
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+if
+    ((a % 2 == 0) && (b % 2 ==0))
+{
+    Console.WriteLine(a + b);
+}
+else if
+    ((a % 2 != 2) || (b % 2 != 0)) 
+{
+    Console.WriteLine("Insert even numbers");
+}
