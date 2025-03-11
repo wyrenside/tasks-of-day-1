148bc08a519c72bdb1b063bea6ae9d90c11bcdc8
wyrenside <haciyev004@icloud.com>
2025-03-11 23:19:03 +0400

    sixth task

---
+++ task 6/task 6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+int num = int.Parse(Console.ReadLine());
+if
+    ((num / 10) > (num % 10))
+{
+    Console.WriteLine(num / 10);
+}
+else if
+    ((num / 10) < (num % 10))
+{
+    Console.WriteLine(num % 10);
+}
+else
+{
+    Console.WriteLine("Digits are equal");
+}
