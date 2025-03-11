d8fb7e3a3af55dc0c495ee9103c866b20b8bf064
wyrenside <haciyev004@icloud.com>
2025-03-11 23:16:26 +0400

    second task

---
+++ task 2/task 2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.Design;
+
+int num = int.Parse(Console.ReadLine());
+if
+    (num % 2 == 0)
+{
+    Console.WriteLine("The given number is even");
+}
+else if
+    (num % 2 != 0)
+    {
+    Console.WriteLine("The given number is odd");
+}
