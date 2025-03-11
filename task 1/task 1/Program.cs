0c36eb946e4dc03879056c8aa485803337fef577
wyrenside <haciyev004@icloud.com>
2025-03-11 23:12:43 +0400

    first task

---
+++ task 1/task 1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.Design;
+
+string username = Console.ReadLine();
+string password = Console.ReadLine();
+if
+    (username == "admin" && password == "Admin123") 
+{
+    Console.WriteLine("Welcome");
+}
+else if
+    (username != "admin" || password != "Admin123")
+{
+    Console.WriteLine("Username or password is incorrect");
+}
