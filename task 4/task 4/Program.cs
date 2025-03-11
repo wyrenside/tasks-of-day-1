f1ab0dbfb3b7046fa56b10f4b107e0a7b6691816
wyrenside <haciyev004@icloud.com>
2025-03-11 23:17:44 +0400

    fourth task

---
+++ task 4/task 4/Program.cs
@@ -0,0 +1,36 @@
+﻿int Score = int.Parse(Console.ReadLine());
+if
+     ((Score > 100) && (Score < 0)) 
+{
+    Console.WriteLine("That ammount of score is not possible");
+}
+else if
+    (Score >= 91)
+{
+    Console.WriteLine("A");
+}
+else if
+    (Score >= 81)
+{
+    Console.WriteLine("B");
+}
+else if
+    (Score >= 71)
+{
+    Console.WriteLine("C");
+}
+else if
+    (Score >= 61)
+{
+    Console.WriteLine("D");
+}
+else if
+    (Score >= 51)
+{
+    Console.WriteLine("E");
+}
+else if
+    (Score < 51)
+{
+    Console.WriteLine("F");
+}
