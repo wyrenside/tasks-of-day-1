12f4ed861681f45d1daa0bd2e4b77ac0f369bf55
wyrenside <haciyev004@icloud.com>
2025-03-13 23:36:36 +0400

    1ci

---
+++ Firsttasks/Firsttasks/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Firsttasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            string password = Console.ReadLine();
+            if
+                (username == "admin" && password == "Admin123")
+            {
+                Console.WriteLine("Welcome");
+            }
+            else if
+                (username != "admin" || password != "Admin123")
+            {
+                Console.WriteLine("Username or password is incorrect");
+            }
+
+        }
+    }
+}
